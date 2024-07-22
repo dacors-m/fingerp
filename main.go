c37a53471f35a1f35bf78ccc48aae63ca4590bdc
William David Cortes Mantilla <dcortesmanti@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2024-07-21 20:40:50 -0500

    test

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/dacors-m/fingerp/cmd"
+import (
+	"github.com/dacors-m/fingerp/cmd"
+)
 
 func main() {
 	cmd.Execute()
